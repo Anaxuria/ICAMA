25055b6fd79c4548d8ef1a256c7dcada782416a7
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-04-20 17:41:26 -0300

    update menu page

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('.slick-carousel').slick({
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true
+  });
+});
+
+$(document).ready(function(){
+    $('.slick-carousel2').slick({
       slidesToShow: 4,
       slidesToScroll: 4,
       autoplay: true,
